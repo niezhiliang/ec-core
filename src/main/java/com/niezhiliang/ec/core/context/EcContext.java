2bd338f820b08539a3c93081534f2a1a12ef348e
niezhiliang <1132263176@qq.com>
2018-07-05 11:23:37 +0800

    m

--- src/main/java/com/niezhiliang/ec/core/context/EcContext.java
+++ src/main/java/com/niezhiliang/ec/core/context/EcContext.java
@@ -1,18 +1,17 @@
 package com.niezhiliang.ec.core.context;
 
 import com.niezhiliang.ec.core.entity.ParentParams;
-import com.niezhiliang.ec.core.entity.ParentReturn;
 import com.niezhiliang.ec.core.pattern.Behave;
 
-public class EcContext<T extends ParentParams,K extends ParentReturn> {
+public class EcContext<T extends ParentParams> {
     private Behave behave;
 
     public EcContext(Behave behave) {
         this.behave = behave;
     }
 
-    public K doJob(T t) {
-        return  behave.doSomeJob(t);
+    public <K>K doJob(T t) {
+        return (K) behave.doSomeJob(t);
     }
 }
 
--- src/main/java/com/niezhiliang/ec/core/strategy/GenerateCertStrategy.java
+++ src/main/java/com/niezhiliang/ec/core/strategy/GenerateCertStrategy.java
@@ -2,25 +2,23 @@ package com.niezhiliang.ec.core.strategy;
 
 import com.niezhiliang.ec.core.context.EcContext;
 import com.niezhiliang.ec.core.entity.CertParams;
-import com.niezhiliang.ec.core.entity.CertReturn;
 import com.niezhiliang.ec.core.entity.KestoreParams;
 import com.niezhiliang.ec.core.entity.KestoreReturn;
 import com.niezhiliang.ec.core.pattern.Behave;
 
-public class GenerateCertStrategy implements Behave<CertParams,CertReturn> {
+public class GenerateCertStrategy implements Behave<CertParams,KestoreReturn>{
+
+
     @Override
-    public CertReturn doSomeJob(CertParams certParams) {
-       CertReturn certReturn = new CertReturn();
-       certReturn.setA("hello");
-       certReturn.setB("World");
-       return certReturn;
+    public KestoreReturn doSomeJob(CertParams certParams) {
+        KestoreReturn parentReturn = new KestoreReturn();
+         parentReturn.setA("哈哈");
+        return parentReturn;
     }
 
     public static void main(String[] args) {
         EcContext ecContext = new EcContext(new GenerateKstoreStrategy());
-        KestoreReturn kestoreReturn = ecContext.doJob(new KestoreParams());
-
-        ecContext = new EcContext(new GenerateCertStrategy());
-        ecContext.doJob(new CertParams());
+        KestoreReturn kestoreReturn = (KestoreReturn) ecContext.doJob(new KestoreParams());
+        System.out.println(kestoreReturn.getA());
     }
 }
